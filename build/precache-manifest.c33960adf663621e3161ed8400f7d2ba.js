fcf8d36dcd66781b3bca72f5b9f1288484f52704
Jax Hodgkinson <jax.h@outlook.com>
2021-12-29 21:15:52 -0500

    Built for github pages

---
+++ build/precache-manifest.c33960adf663621e3161ed8400f7d2ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c29de0bee7aca2d41e25",
+    "url": "/matic-tip-jar/static/css/main.2581624e.chunk.css"
+  },
+  {
+    "revision": "c29de0bee7aca2d41e25",
+    "url": "/matic-tip-jar/static/js/main.c29de0be.chunk.js"
+  },
+  {
+    "revision": "ec99dfb45598b0cf0242",
+    "url": "/matic-tip-jar/static/css/1.434f11cf.chunk.css"
+  },
+  {
+    "revision": "ec99dfb45598b0cf0242",
+    "url": "/matic-tip-jar/static/js/1.ec99dfb4.chunk.js"
+  },
+  {
+    "revision": "95348d44f79e2fe089fb",
+    "url": "/matic-tip-jar/static/js/runtime~main.95348d44.js"
+  },
+  {
+    "revision": "33e19ff3c3cfb76d7016d32b15ef3c15",
+    "url": "/matic-tip-jar/index.html"
+  }
+];
